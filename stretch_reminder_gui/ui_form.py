c7a755de0a76878e07dc38e64277dcd2a5b5ee28
b-s-code <89502375+b-s-code@users.noreply.github.com>
2025-05-02 21:30:00 +0800

    Add GUI widgets.

---
+++ stretch_reminder_gui/ui_form.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+
+################################################################################
+## Form generated from reading UI file 'form.ui'
+##
+## Created by: Qt User Interface Compiler version 6.9.0
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
+
+from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
+    QMetaObject, QObject, QPoint, QRect,
+    QSize, QTime, QUrl, Qt)
+from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
+    QFont, QFontDatabase, QGradient, QIcon,
+    QImage, QKeySequence, QLinearGradient, QPainter,
+    QPalette, QPixmap, QRadialGradient, QTransform)
+from PySide6.QtWidgets import (QApplication, QLabel, QProgressBar, QSizePolicy,
+    QWidget)
+
+class Ui_Widget(object):
+    def setupUi(self, Widget):
+        if not Widget.objectName():
+            Widget.setObjectName(u"Widget")
+        Widget.resize(614, 329)
+        self.label = QLabel(Widget)
+        self.label.setObjectName(u"label")
+        self.label.setGeometry(QRect(150, 40, 341, 91))
+        font = QFont()
+        font.setPointSize(72)
+        self.label.setFont(font)
+        self.progressBar = QProgressBar(Widget)
+        self.progressBar.setObjectName(u"progressBar")
+        self.progressBar.setGeometry(QRect(110, 160, 381, 81))
+        font1 = QFont()
+        font1.setPointSize(10)
+        self.progressBar.setFont(font1)
+        self.progressBar.setValue(0)
+
+        self.retranslateUi(Widget)
+
+        QMetaObject.connectSlotsByName(Widget)
+    # setupUi
+
+    def retranslateUi(self, Widget):
+        Widget.setWindowTitle(QCoreApplication.translate("Widget", u"Widget", None))
+        self.label.setText(QCoreApplication.translate("Widget", u"stretch", None))
+    # retranslateUi
+
